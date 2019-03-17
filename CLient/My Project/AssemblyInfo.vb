3f1825a2a07151ab96470041fdde9fec9155feff
Jay Asbury <vbjay.net@gmail.com>
2019-03-17 10:02:25 -0400

    Add Sample client

---
+++ CLient/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CLient")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("VBJay Solutions")>
+<Assembly: AssemblyProduct("CLient")>
+<Assembly: AssemblyCopyright("Copyright © VBJay Solutions 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f4f0558b-f325-482a-8809-f681bd72e3ac")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
