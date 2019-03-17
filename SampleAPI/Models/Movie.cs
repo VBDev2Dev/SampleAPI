b8d98c5d6a9dbc5bcda31072c7c128a6b57888c3
Jay Asbury <vbjay.net@gmail.com>
2019-03-17 09:12:13 -0400

    Add Model class with data annotations

---
+++ SampleAPI/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SampleAPI.Models
+{
+	 public class Movie
+	 {
+		  [Key]
+		  [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		  public long ID { get; set; }
+		  [Required]
+		  [StringLength(80)]
+		  public string Title { get; set; }
+		  public DateTime? ReleaseDate { get; set; }
+		  [StringLength(25)]
+		  public string Genre { get; set; }
+		  [Range(0.0,double.MaxValue)]
+		  public decimal Price { get; set; }
+	 }
+}
