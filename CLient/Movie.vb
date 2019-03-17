c289c043ca45468d01832b7f171e9b32bfb723b0
Jay Asbury <vbjay.net@gmail.com>
2019-03-17 12:30:02 -0400

    Add movie class and datasource to client

---
+++ CLient/Movie.vb
@@ -0,0 +1,7 @@
+﻿Public Class Movie
+	 Public Property ID As Long
+	 Public Property Title As String
+	 Public Property ReleaseDate As DateTime?
+	 Public Property Genre As String
+	 Public Property Price As Decimal
+End Class
